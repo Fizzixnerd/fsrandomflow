2bd838b2e1c547ee93714f6a5e52d14e38f589ae
Barend Venter <barendventer@gmail.com>
2017-07-21 21:16:39 -0700

    Move to .NET 4.6.1

--- lib/FsRandomFlow/AssemblyInfo.fs
+++ lib/FsRandomFlow/AssemblyInfo.fs
@@ -1,6 +1,5 @@
-﻿namespace fsrandomflow.AssemblyInfo
+﻿namespace FsRandomFlow.AssemblyInfo
 
-open System.Resources
 open System.Reflection
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
@@ -8,15 +7,22 @@ open System.Runtime.InteropServices
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[<assembly: AssemblyTitle("fsrandomflow")>]
+[<assembly: AssemblyTitle("FsRandomFlow")>]
 [<assembly: AssemblyDescription("")>]
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
-[<assembly: AssemblyProduct("fsrandomflow")>]
-[<assembly: AssemblyCopyright("Copyright ©  2017")>]
+[<assembly: AssemblyProduct("FsRandomFlow")>]
+[<assembly: AssemblyCopyright("Copyright © Barend Venter, 2017")>]
 [<assembly: AssemblyTrademark("")>]
 [<assembly: AssemblyCulture("")>]
-[<assembly: NeutralResourcesLanguage("en")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("9cf1461c-8e8d-41f5-b7fd-42c853eefa72")>]
 
 // Version information for an assembly consists of the following four values:
 // 
@@ -28,8 +34,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("1.0.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0.0")>]
+[<assembly: AssemblyVersion("0.0.1.0")>]
+[<assembly: AssemblyFileVersion("0.0.0.0")>]
 
 do
     ()
