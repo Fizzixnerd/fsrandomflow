906d197cdba3fe8fbb03d012a2f1e7ccf55e04e2
Barend Venter <barendventer@gmail.com>
2017-07-30 05:20:21 -0700

    Fixed the tests

--- lib/FsRandomFlow/AssemblyInfo.fs
+++ lib/FsRandomFlow/AssemblyInfo.fs
@@ -19,7 +19,7 @@ open System.Runtime.InteropServices
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[<assembly: ComVisible(true)>]
+[<assembly: ComVisible(false)>]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [<assembly: Guid("9cf1461c-8e8d-41f5-b7fd-42c853eefa72")>]
--- test/Canaries/Canaries.fs
+++ test/Canaries/Canaries.fs
@@ -1,11 +1,9 @@
-﻿namespace Canaries
+﻿namespace FsRandomFlowTests
 
 module Canaries = 
-    open FsCheck
-    open FsCheck.Xunit
+    open Xunit
     open FsRandomFlow
     open FsRandomFlow.RVar
-    open Xunit
 
     let seeds = RVar.take 4 StdUniform
                 |> RVar.runrvar 123456789
@@ -17,16 +15,16 @@ module Canaries =
     //Distributions
 
     [<Fact>]
-    let canaryStdUniform = [|1287638892; 1840282602; 928205646; 470358147|] = seeds
+    let canaryStdUniform () = [|1287638892; 1840282602; 928205646; 470358147|] = seeds
 
     [<Fact>]
-    let canaryStdNormal = [|-0.3205875829; -1.323026481; -0.4755294123; 0.3722125042|] = testRun (RVar.Normal(0.0,1.0))
+    let canaryStdNormal () = [|-0.3205875829; -1.323026481; -0.4755294123; 0.3722125042|] = testRun (RVar.Normal(0.0,1.0))
 
     [<Fact>]
-    let canaryExponential = [|0.2985867874; 1.753716246; 8.98085485; 0.15788101|] = testRun (RVar.Exponential(0.5))
+    let canaryExponential () = [|0.2985867874; 1.753716246; 8.98085485; 0.15788101|] = testRun (RVar.Exponential(0.5))
 
     [<Fact>]
-    let canaryWeibull = [|0.0111442587; 0.3844400839; 10.08196923; 0.003115801664|] = testRun (RVar.Weibull(0.5,0.5))
+    let canaryWeibull () = [|0.0111442587; 0.3844400839; 10.08196923; 0.003115801664|] = testRun (RVar.Weibull(0.5,0.5))
 
     [<Fact>]
-    let canaryPoisson = [|8; 6; 2; 2|] = testRun (RVar.Poisson(5.5))
+    let canaryPoisson () = [|8; 6; 2; 2|] = testRun (RVar.Poisson(5.5))
