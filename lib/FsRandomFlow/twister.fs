bb17a32a479a7dab6c01ac51de6830270aeb58b3
Barend Venter <barendventer@gmail.com>
2017-08-01 05:35:18 -0700

    Parameterized TwistGen (prep for dyncreate)

--- lib/FsRandomFlow/twister.fs
+++ lib/FsRandomFlow/twister.fs
@@ -18,80 +18,54 @@ module Twister =
 
     //From wikipedia - heavily based on the Haskell example there
 
-    ///Word Size
-    let w = 32
-    ///Degree of Recurrence
-    let n = 624
-    ///Middle word
-    let m = 397
-    ///Separation point
-    let r = 31
-    ///Coefficients of the rational normal form twist matrix
-    let a = 0x9908B0DF
-    ///TGFSR(R) Tempering Bitmask
-    let b = 0x9D2C5680
-    ///TGFSR(R) Tempering Bitmask
-    let c = 0xEFC60000
-    ///TGFSR(R) Tempering Bit Shift
-    let s = 7
-    ///TGFSR(R) Tempering Bit Shift
-    let t = 15
-    ///Mersenne Twister Tempering Bit Mask
-    let d = 0xFFFFFFFF
-    ///Mersenne Twister Tempering Bit Shift
-    let u = 11
-    ///Mersenne Twister Tempering Bit Shift
-    let l = 18
-    ///Additional parameter used in state initialization
-    let f = 1812433253
 
-    // These will be used to create a streaming defn', w/ buffer
-    let nextState mword word0 word1 = 
-        let upper x = (x >>> r) <<< r
-        let lower x = (x <<< r) >>> r
-        let multA x = if (x &&& 1) = 0 then (x >>> 1) else (x >>> 1) ^^^ a
-        mword ^^^ multA (upper(word0) ||| lower(word1))
-
-    let getValue lword = 
-        let mutable x = lword
-        x <- x ^^^ ((x >>> u) &&& d)
-        x <- x ^^^ ((x <<< s) &&& b)
-        x <- x ^^^ ((x <<< t) &&& c)
-        x ^^^ (x >>> l)
-      
-    // Initializes the state array for mersenne twister
-    let initSeq seed = 
-        (seed,0)
-        |> Seq.unfold(
-            fun (prev,i) ->
-                if i < n
-                then
-                    let next = f * (prev ^^^ (prev >>> (w-2))) + i
-                    Some(next, (next,i+1))
-                else None
-                )
-        |> Seq.toArray
-
-    // Resets an array in place
-    let resetBuffer seed (arr : int array) = 
-        (seed,0)
-        |> Seq.unfold(
-            fun (prev,i) ->
-                    if i < n
-                    then
-                        let next = f * (prev ^^^ (prev >>> (w-2))) + i
-                        Some(next, (next,i+1))
-                    else None
-                )
-        |> Seq.iteri(fun v i -> arr.[i] <- v)
 
     // A proper modulo, since F# % is the remainder function
     let modulus x m =
         if x > 0 then x % m
         else (m + (x % m)) % m
-
+        
     // This is the write head for a particular mersenne twister thread
-    type TwistGen(seed) =
+    type TwistGen(seed,w,n,m,r,a,b,c,s,t,d,u,l,f) =
+        // These will be used to create a streaming defn', w/ buffer
+        let nextState mword word0 word1 = 
+            let upper x = (x >>> r) <<< r
+            let lower x = (x <<< r) >>> r
+            let multA x = if (x &&& 1) = 0 then (x >>> 1) else (x >>> 1) ^^^ a
+            mword ^^^ multA (upper(word0) ||| lower(word1))
+
+        let getValue lword = 
+            let mutable x = lword
+            x <- x ^^^ ((x >>> u) &&& d)
+            x <- x ^^^ ((x <<< s) &&& b)
+            x <- x ^^^ ((x <<< t) &&& c)
+            x ^^^ (x >>> l)
+      
+        // Initializes the state array for mersenne twister
+        let initSeq seed = 
+            (seed,0)
+            |> Seq.unfold(
+                fun (prev,i) ->
+                    if i < n
+                    then
+                        let next = f * (prev ^^^ (prev >>> (w-2))) + i
+                        Some(next, (next,i+1))
+                    else None
+                    )
+            |> Seq.toArray
+
+        // Resets an array in place
+        let resetBuffer seed (arr : int array) = 
+            (seed,0)
+            |> Seq.unfold(
+                fun (prev,i) ->
+                        if i < n
+                        then
+                            let next = f * (prev ^^^ (prev >>> (w-2))) + i
+                            Some(next, (next,i+1))
+                        else None
+                    )
+            |> Seq.iteri(fun v i -> arr.[i] <- v)
         let state = initSeq seed
         let mutable head = 0
         member this.ixRelative offset = 
@@ -121,13 +95,43 @@ module Twister =
                head <- 0
         interface IEnumerator<int> with 
             override this.get_Current () = this.Current
+    
+    let standardGen(seed) = 
+        ///Word Size
+        let w = 32
+        ///Degree of Recurrence
+        let n = 624
+        ///Middle word
+        let m = 397
+        ///Separation point
+        let r = 31
+        ///Coefficients of the rational normal form twist matrix
+        let a = 0x9908B0DF
+        ///TGFSR(R) Tempering Bitmask
+        let b = 0x9D2C5680
+        ///TGFSR(R) Tempering Bitmask
+        let c = 0xEFC60000
+        ///TGFSR(R) Tempering Bit Shift
+        let s = 7
+        ///TGFSR(R) Tempering Bit Shift
+        let t = 15
+        ///Mersenne Twister Tempering Bit Mask
+        let d = 0xFFFFFFFF
+        ///Mersenne Twister Tempering Bit Shift
+        let u = 11
+        ///Mersenne Twister Tempering Bit Shift
+        let l = 18
+        ///Additional parameter used in state initialization
+        let f = 1812433253
+        new TwistGen(seed,w,n,m,r,a,b,c,s,t,d,u,l,f)
 
     type Twister(seed) =
         interface IEnumerable with
             member this.GetEnumerator() =
-                new TwistGen(seed) :> IEnumerator
+                standardGen(seed) :> IEnumerator
         interface IEnumerable<int> with
             member this.GetEnumerator() =
-                new TwistGen(seed) :> IEnumerator<int>
+                standardGen(seed) :> IEnumerator<int>
 
     let twister seed = Twister(seed) :> IEnumerable<int>
+
--- lib/FsRandomFlow/twister.fsi
+++ lib/FsRandomFlow/twister.fsi
@@ -2,5 +2,24 @@
 //This code liscensed under the MIT license, see LICENSE
 
 namespace FsRandomFlow
+    //TODO: More informative interface than IEnumerator/IEnumerable
+    //[<Interface>]
+    //type ITwister = 
+    //    member freeze : TwisterFrozen
+    //    member thaw : TwisterThawed
+    //    member peek : int
+    //    member branchWithID : int -> TwisterFrozen
+    //and
+    //    [<Class>]
+    //    TwisterThawed = 
+    //        interface ITwister
+    //        member branch : TwisterFrozen
+    //        member next : int
+    //and 
+    //    [<Class>]
+    //    TwisterFrozen = 
+    //        interface ITwister
+    //        member next : int * TwisterFrozen
+
     module Twister =
         val twister : int -> int seq
